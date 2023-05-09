94267254560b87f8e5be55f5a7c95843f61125df
yu00212 <yu2000212@icloud.com>
2023-05-09 22:45:41 +0900

    テーブル構造修正

--- database/migrations/2023_05_05_025209_create_files_table.php
+++ database/migrations/2023_05_05_025209_create_files_table.php
@@ -14,11 +14,10 @@ return new class extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->id(); // 自動採番されるファイルID
-            $table->integer('user_id')->comment('ファイルをアップロードしたユーザーのID');
             $table->string('name')->comment('ファイル名');
             $table->string('path')->comment('ファイルの保存先パス');
-            $table->integer('size')->comment('ファイルサイズ');
-            $table->string('mime_type')->comment('ファイルにMIMEタイプ');
+            $table->integer('size')->default(0)->comment('ファイルサイズ');
+            $table->string('mime_type')->nullable()->comment('ファイルにMIMEタイプ');
             $table->timestamps();
         });
     }
